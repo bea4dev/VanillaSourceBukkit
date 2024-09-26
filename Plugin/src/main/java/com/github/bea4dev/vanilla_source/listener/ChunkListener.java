4952d17cd13461f9de17ada9f3b37ba4e67fb0bc
bea4dev <bea0224@outlook.jp>
2024-09-27 00:14:11 +0900

    commit

--- Plugin/src/main/java/com/github/bea4dev/vanilla_source/listener/ChunkListener.java
+++ Plugin/src/main/java/com/github/bea4dev/vanilla_source/listener/ChunkListener.java
@@ -1,6 +1,9 @@
 package com.github.bea4dev.vanilla_source.listener;
 
+import com.github.bea4dev.vanilla_source.VanillaSource;
 import com.github.bea4dev.vanilla_source.api.world.cache.AsyncWorldCache;
+import com.github.bea4dev.vanilla_source.util.TaskHandler;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,8 +12,29 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChunkListener implements Listener {
-    
+
+    private final List<Chunk> updateChunks = new ArrayList<>();
+
+    public ChunkListener() {
+        Bukkit.getScheduler().runTaskTimer(VanillaSource.getPlugin(), () -> {
+            for (var chunk : updateChunks) {
+                TaskHandler.runWorldSync(chunk.getWorld(), () -> AsyncWorldCache.update(chunk));
+            }
+            updateChunks.clear();
+        }, 0, 5);
+    }
+
+    private void registerUpdateChunk(Chunk chunk) {
+        if (updateChunks.contains(chunk)) {
+            return;
+        }
+        updateChunks.add(chunk);
+    }
+
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent event){
         Chunk chunk = event.getChunk();
@@ -20,13 +44,13 @@ public class ChunkListener implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event){
         Chunk chunk = event.getBlock().getChunk();
-        AsyncWorldCache.update(chunk);
+        registerUpdateChunk(chunk);
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event){
         Chunk chunk = event.getBlock().getChunk();
-        AsyncWorldCache.update(chunk);
+        registerUpdateChunk(chunk);
     }
 
     @EventHandler
