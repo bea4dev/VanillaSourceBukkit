bb0129aa1fa019e2c335a0a2a47d19e34c54f2bb
bea4dev <bea0224@outlook.jp>
2024-09-27 00:14:11 +0900

    commit

--- Plugin/src/main/java/com/github/bea4dev/vanilla_source/util/TaskHandler.java
+++ Plugin/src/main/java/com/github/bea4dev/vanilla_source/util/TaskHandler.java
@@ -9,44 +9,44 @@ import world.chiyogami.chiyogamilib.scheduler.WorldThreadRunnable;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
-public class TaskHandler{
+public class TaskHandler {
     
-    public static <U> CompletableFuture<U> supplySync(Supplier<U> supplier){
+    public static <U> CompletableFuture<U> supplySync(Supplier<U> supplier) {
         CompletableFuture<U> completableFuture = new CompletableFuture<>();
         new BukkitTaskHandler<>(completableFuture, supplier, false).runTask(VanillaSource.getPlugin());
         
         return completableFuture;
     }
     
-    public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier){
+    public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier) {
         CompletableFuture<U> completableFuture = new CompletableFuture<>();
         new BukkitTaskHandler<>(completableFuture, supplier, true).runTaskAsynchronously(VanillaSource.getPlugin());
         
         return completableFuture;
     }
     
-    public static <U> CompletableFuture<U> supplyWorldSync(World world, Supplier<U> supplier){
+    public static <U> CompletableFuture<U> supplyWorldSync(World world, Supplier<U> supplier) {
         CompletableFuture<U> completableFuture = new CompletableFuture<>();
         new WorldTaskHandler<>(completableFuture, supplier, world).runTask(VanillaSource.getPlugin());
         
         return completableFuture;
     }
     
-    public static void runSync(Runnable runnable){
+    public static void runSync(Runnable runnable) {
         Bukkit.getScheduler().runTask(VanillaSource.getPlugin(), runnable);
     }
     
-    public static void runAsync(Runnable runnable){
+    public static void runAsync(Runnable runnable) {
         Bukkit.getScheduler().runTaskAsynchronously(VanillaSource.getPlugin(), runnable);
     }
     
-    public static void runAsyncImmediately(Runnable runnable){
+    public static void runAsyncImmediately(Runnable runnable) {
         Thread thread = new Thread(runnable);
         thread.setDaemon(true);
         thread.start();
     }
     
-    public static void runWorldSync(World world, Runnable runnable){
+    public static void runWorldSync(World world, Runnable runnable) {
         new WorldThreadRunnable(world){
             @Override
             public void run() {
@@ -56,12 +56,12 @@ public class TaskHandler{
     }
     
     
-    private static class BukkitTaskHandler<T> extends BukkitRunnable{
+    private static class BukkitTaskHandler<T> extends BukkitRunnable {
         private final CompletableFuture<T> completableFuture;
         private final Supplier<T> supplier;
         private final boolean isAsync;
         
-        private BukkitTaskHandler(CompletableFuture<T> completableFuture, Supplier<T> supplier, boolean isAsync){
+        private BukkitTaskHandler(CompletableFuture<T> completableFuture, Supplier<T> supplier, boolean isAsync) {
             this.completableFuture = completableFuture;
             this.supplier = supplier;
             this.isAsync = isAsync;
@@ -72,16 +72,16 @@ public class TaskHandler{
             T result = supplier.get();
             
             Runnable runnable = () -> completableFuture.complete(result);
-            if(isAsync){
+            if (isAsync) {
                 TaskHandler.runSync(runnable);
-            }else{
+            } else {
                 TaskHandler.runAsync(runnable);
             }
         }
     }
     
     
-    private static class WorldTaskHandler<T> extends WorldThreadRunnable{
+    private static class WorldTaskHandler<T> extends WorldThreadRunnable {
         private final CompletableFuture<T> completableFuture;
         private final Supplier<T> supplier;
         
